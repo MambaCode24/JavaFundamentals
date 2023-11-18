65f02078ed74be8cfa6d5444dd1289690562af08
Joni Lacej <jonilacej75@gmail.com>
2023-11-18 19:37:57 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = ("My name is Coding Dojo");
+        String myAge = ("I am 100 years old");
+        String myHometown = ("My hometown is Burbank, CA");
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHometown);
+
+        }
+    }
